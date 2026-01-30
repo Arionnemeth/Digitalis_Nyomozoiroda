d8db20e52acdedf78e7614ef4b9b51a6ef567cee
Arion <Arion@LENOVO-ARION>
2026-01-30 10:02:17 +0100

    Idovonal esemeny class kész, ToStringgel

---
+++ Digitalis_Nyomozoiroda/Idovonal_esemeny.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitalis_Nyomozoiroda
+{
+    internal class Idovonal_esemeny
+    {
+        private DateTime datum;
+        private string esemeny;
+
+        public Idovonal_esemeny(DateTime datum, string esemeny)
+        {
+            this.datum = datum;
+            this.esemeny = esemeny;
+        }
+
+        public DateTime Datum { get => datum; set => datum = value; }
+        public string Esemeny { get => esemeny; set => esemeny = value; }
+
+        public override string ToString()
+        {
+            return $"{this.datum}: {this.esemeny}";
+        }
+    }
+}
