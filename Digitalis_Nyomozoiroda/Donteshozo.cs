0f2d04908a0e64e7d61eb0107444f3be37145486
Arion <Arion@LENOVO-ARION>
2026-01-30 10:32:40 +0100

    Donteshozo metodus class elkeszult

---
+++ Digitalis_Nyomozoiroda/Donteshozo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitalis_Nyomozoiroda
+{
+    internal class Donteshozo
+    {
+        public Donteshozo(Gyanusitott g, Bizonyitek b)
+        {
+            if (b.Megbizhatosagi_ertek >= 3)
+            {
+                g.Gyanusitottsagi_szint += 10;
+            }
+            if (g.Gyanusitottsagi_szint >= 70)
+            {
+                Console.WriteLine("Figyelem elérte a felső szintet a gyanusítottságban!");
+            }
+        }
+       
+    }
+}
