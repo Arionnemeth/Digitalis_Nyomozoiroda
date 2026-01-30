5b4817e1e4e90423b8bd39f645b59fda8e892cff
Arion <Arion@LENOVO-ARION>
2026-01-30 08:58:36 +0100

    Felhasznalo class kesz, ToStringgel egybekötve

---
+++ Digitalis_Nyomozoiroda/Felhasznalo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitalis_Nyomozoiroda
+{
+    internal class Felhasznalo
+    {
+        private string nev;
+        private int azonosito;
+        private string szerepkor;
+
+        public Felhasznalo(string nev, int azonosito, string szerepkor)
+        {
+            this.nev = nev;
+            this.azonosito = azonosito;
+            this.szerepkor = szerepkor;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Azonosito { get => azonosito; set => azonosito = value; }
+        public string Szerepkor { get => szerepkor; set => szerepkor = value; }
+
+        public override string ToString()
+        {
+            return $"{this.nev}  {this.szerepkor} azonosítója:  {this.azonosito}";
+        }
+    }
+}
