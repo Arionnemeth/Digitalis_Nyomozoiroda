9b25e5442f6e8b6fc4f887d50321ab07f5d79dbb
Arion <Arion@LENOVO-ARION>
2026-01-30 08:51:29 +0100

    Add project files.

---
+++ Digitalis_Nyomozoiroda/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Digitalis_Nyomozoiroda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
