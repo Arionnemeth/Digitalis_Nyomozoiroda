89b5018fc73892b787988f45016d79679457f53e
Arion <Arion@LENOVO-ARION>
2026-01-30 10:59:32 +0100

    Adattar osztaly megcsinalva

---
+++ Digitalis_Nyomozoiroda/Adattar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitalis_Nyomozoiroda
+{
+    internal class Adattar
+    {
+        List<Felhasznalo> felhasznalolista;
+        List<Ugy> ugyeklista;
+        List<Szemely> szemelylista;
+        List<Bizonyitek> bizonyiteklista;
+
+        public Adattar(List<Felhasznalo> felhasznalolista, List<Ugy> ugyeklista, List<Szemely> szemelylista, List<Bizonyitek> bizonyiteklista)
+        {
+            this.felhasznalolista = new List<Felhasznalo> ();
+            this.ugyeklista = new List<Ugy> ();
+            this.szemelylista = new List<Szemely> ();
+            this.bizonyiteklista = new List<Bizonyitek> ();
+        }
+
+        internal List<Felhasznalo> Felhasznalolista { get => felhasznalolista; set => felhasznalolista = value; }
+        internal List<Ugy> Ugyeklista { get => ugyeklista; set => ugyeklista = value; }
+        internal List<Szemely> Szemelylista { get => szemelylista; set => szemelylista = value; }
+        internal List<Bizonyitek> Bizonyiteklista { get => bizonyiteklista; set => bizonyiteklista = value; }
+    }
+}
