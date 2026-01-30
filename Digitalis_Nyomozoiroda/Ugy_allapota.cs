eff9b5f8f7a77be79008247a267b4919f2cf6cd3
Arion <Arion@LENOVO-ARION>
2026-01-30 09:30:27 +0100

    Ugy allapota félig kész verziója

---
+++ Digitalis_Nyomozoiroda/Ugy_allapota.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitalis_Nyomozoiroda
+{
+    internal class Ugy_allapota
+    {
+        private string aktualis_statusz;
+
+        public Ugy_allapota(string aktualis_statusz)
+        {
+            this.aktualis_statusz = aktualis_statusz;
+        }
+
+        public string Aktualis_statusz { get => aktualis_statusz; set => aktualis_statusz = value; }
+
+        public void AllapotValtoztatas(string allapot)
+        {
+            this.aktualis_statusz = allapot;
+        }
+    }
+}
