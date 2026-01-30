c75bb6d0f1ece7621c52727aaab4878eaef92509
Arion <Arion@LENOVO-ARION>
2026-01-30 10:26:03 +0100

    Ugykezeloben az uj ugyletrehozasa kesz es a listazas is mar csak a szemelyek es bizonyitekok hozzarendelese kell

---
+++ Digitalis_Nyomozoiroda/Ugykezelo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitalis_Nyomozoiroda
+{
+    internal class Ugykezelo
+    {
+        private List<Ugy> ugyek;
+
+        public Ugykezelo(List<Ugy> ugyek)
+        {
+            this.ugyek = new List<Ugy>();
+        }
+
+        internal List<Ugy> Ugyek { get => ugyek; set => ugyek = value; }
+
+        public void Uj_Ugyletrehozasa(Ugy u)
+        {
+            ugyek.Add(u);
+        }
+
+        public void Ugyek_Listazasa()
+        {
+            foreach (var item in ugyek)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
