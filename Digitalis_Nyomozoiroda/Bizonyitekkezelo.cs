006341cdbf7ff402b69b1f357af9d19b7e44d72b
Arion <Arion@LENOVO-ARION>
2026-01-30 10:19:24 +0100

    Bizonyitekkezelo metodusosztaly befejezesre kerult, bizonyitek hozzaadasa és torlese is

--- Digitalis_Nyomozoiroda/Bizonyitekkezelo.cs
+++ Digitalis_Nyomozoiroda/Bizonyitekkezelo.cs
@@ -6,6 +6,22 @@ namespace Digitalis_Nyomozoiroda
 {
     internal class Bizonyitekkezelo
     {
-        private 
+        private List<Bizonyitek> bizonyitekok;
+
+        public Bizonyitekkezelo(List<Bizonyitek> bizonyitekok)
+        {
+            this.bizonyitekok = new List<Bizonyitek>(); 
+        }
+
+        internal List<Bizonyitek> Bizonyitekok { get => bizonyitekok; set => bizonyitekok = value; }
+
+        public void Bizonyitek_hozzaadas(Bizonyitek b)
+        {
+            bizonyitekok.Add(b);
+        }
+        public void Bizonyitek_torlese(Bizonyitek b)
+        {
+            bizonyitekok.Remove(b);
+        }
     }
 }
