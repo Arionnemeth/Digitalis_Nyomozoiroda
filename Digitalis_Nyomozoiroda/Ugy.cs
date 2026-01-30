b8bb2aea3728e17d1a971c4136515e3264181ee0
Arion <Arion@LENOVO-ARION>
2026-01-30 09:17:47 +0100

    Ugy Class elkezdve, listák létrehozva, ToStringekkel együtt

---
+++ Digitalis_Nyomozoiroda/Ugy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitalis_Nyomozoiroda
+{
+    internal class Ugy
+    {
+        private int ugy_azonosito;
+        private string cim;
+        private string leiras;
+        private string allapot;
+        private List<Szemely> resztvevok;
+        private List<Bizonyitek> bizonyitekok;
+
+        public Ugy(int ugy_azonosito, string cim, string leiras, string allapot, List<Szemely> resztvevok, List<Bizonyitek> bizonyitekok)
+        {
+            this.ugy_azonosito = ugy_azonosito;
+            this.cim = cim;
+            this.leiras = leiras;
+            this.allapot = allapot;
+            this.resztvevok = new List<Szemely>();
+            this.bizonyitekok = new List<Bizonyitek>();
+        }
+
+        public int Ugy_azonosito { get => ugy_azonosito; set => ugy_azonosito = value; }
+        public string Cim { get => cim; set => cim = value; }
+        public string Leiras { get => leiras; set => leiras = value; }
+        public string Allapot { get => allapot; set => allapot = value; }
+        internal List<Szemely> Resztvevok { get => resztvevok; set => resztvevok = value; }
+        internal List<Bizonyitek> Bizonyitekok { get => bizonyitekok; set => bizonyitekok = value; }
+
+        public override string ToString()
+        {
+            return $"{this.ugy_azonosito}: {this.cim}: {this.leiras}: {this.allapot}, résztvett személyek: {this.resztvevok}, bizonyitékok: {this.bizonyitekok}";
+        }
+    }
+}
